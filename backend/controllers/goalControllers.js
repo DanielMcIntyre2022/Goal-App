29591a81ee1ebf457087dd3c02e0e870fdd933ff
Daniel <daniel-mcintyre@hotmail.com>
2022-11-07 15:08:14 -0500

    implemented complete CRUD functionality in controllers

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,8 +1,12 @@
 const aysncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalsModel');
+
 const getGoals = aysncHandler(async (req, res) => {
+    const goals = await Goal.find({
+    })
     res.json({
-        message: 'Get Goals'
+       goals
     })
 });
 
@@ -11,21 +15,41 @@ const addGoal = aysncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text feild');
     }
+    const goal = await Goal.create({
+        text: req.body.text
+    })
     res.json({
-        message: 'Add Goals'
+        goal
     })
 });
 
 const updateGoal = aysncHandler(async (req, res) => {
-    res.json({
-        message: `Update goal ${req.params.id}`
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
     })
+
+    res.json(updatedGoal)
 });
 
 const deleteGoal = aysncHandler(async (req, res) => {
-    res.json({
-        message: `Delete goal ${req.params.id}`
-    })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw Error('Goal not found')
+    }
+
+    await Goal.remove(goal)
+
+    res.json({ id: req.params })
 });
 
 module.exports = {
