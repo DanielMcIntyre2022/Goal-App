65006cc0413a292d22291382697ee83ac5119a0b
Daniel <daniel-mcintyre@hotmail.com>
2022-11-07 12:52:21 -0500

    sucessfully made route requests for CRUD operations

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,4 +7,22 @@ router.get('/', (req, res) => {
     })
 });
 
+router.post('/', (req, res) => {
+    res.json({
+        message: 'Post Goals'
+    })
+});
+
+router.put('/:id', (req, res) => {
+    res.json({
+        message: `Update goal ${req.params.id}`
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    res.json({
+        message: `Delete goal ${req.params.id}`
+    })
+});
+
 module.exports = router;
