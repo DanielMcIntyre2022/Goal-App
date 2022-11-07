9a4fcc54b0d2a5431d4a51bc3f21dd6a5bdcf1e0
Daniel <daniel-mcintyre@hotmail.com>
2022-11-07 12:36:34 -0500

    created goals model and connected routes to initial get request

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,10 @@ DB();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.listen(port, console.log('listening on localhost 3023'));
