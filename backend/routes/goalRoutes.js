cc8860503f716ea85c17a70110c3ed3e3a605760
Daniel <daniel-mcintyre@hotmail.com>
2022-11-08 13:53:05 -0500

    protected the goalroutes using middleware and JWT

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getGoals, addGoal, updateGoal, deleteGoal } = require('../controllers/goalControllers');
+const { getGoals, addGoal, updateGoal, deleteGoal 
+} = require('../controllers/goalControllers');
+const { protect } = require('../middleware/authMiddleware');
 
-router.get('/', getGoals);
-router.post('/', addGoal);
-router.put('/:id', updateGoal);
-router.delete('/:id', deleteGoal);
+router.get('/', protect, getGoals);
+router.post('/', protect, addGoal);
+router.put('/:id', protect, updateGoal);
+router.delete('/:id', protect, deleteGoal);
 
 module.exports = router;
