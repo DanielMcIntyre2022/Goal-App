faa968d3019afee4d26d0ffcc13b485fba4914fa
Daniel <daniel-mcintyre@hotmail.com>
2022-11-10 17:01:57 -0500

    created GoalForm component and added state input fields to connect to goalSlice redux state

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import GoalForm from '../components/GoalForm';
 
 function Dashboard() {
 
@@ -14,9 +15,13 @@ function Dashboard() {
     }
   },[user, navigate]);
 
-  return (
-    <div>Dashboard</div>
-  )
+  return <>
+    <section className='heading'>
+      <h1>Welcome! {user && user.name}</h1>
+      <p>Goals Dashboard</p>
+    </section>
+    <GoalForm/>
+  </>
 }
 
 export default Dashboard
