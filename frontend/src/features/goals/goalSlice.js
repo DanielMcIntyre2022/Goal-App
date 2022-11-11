59e579bc4117dc38f1be49d687e4416e248858be
Daniel <daniel-mcintyre@hotmail.com>
2022-11-11 11:01:31 -0500

    succesfully fetched goals from database

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -21,6 +21,20 @@ export const createGoal = createAsyncThunk('goals/create', async(goalData, thunk
             error.response.data.message) || error.message || error.toString()
         return thunkAPI.rejectWithValue(message)
     }
+});
+
+// Get User Goals //
+
+export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalService.getGoals(token)
+    } catch (error) {
+        const message = (error.response && 
+            error.response && 
+            error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
 })
 
 export const goalSlice = createSlice({
@@ -44,6 +58,19 @@ export const goalSlice = createSlice({
           state.isError = true
           state.message = action.payload
         })
+        .addCase(getGoals.pending, (state) => {
+            state.isLoading = true
+          })
+          .addCase(getGoals.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.goals = action.payload
+          })
+          .addCase(getGoals.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+          })
     }
 });
 
